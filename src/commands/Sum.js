aef91b74c5024b8a9c5833a4c2952e6f52aa6ee2
viktoria <kingsviktory@gmail.com>
2024-02-23 13:48:04 +0300

    feat: add config

---
+++ src/commands/Sum.js
@@ -0,0 +1,11 @@
+export default class Sum {
+    constructor(valToAdd) {
+        this.valToAdd = valToAdd
+    }
+    execute(currVal) {
+        return currVal + this.valToAdd
+    }
+    undo(currVal) {
+        return currVal - this.valToAdd
+    }
+}
