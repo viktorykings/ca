db34625c309e846d546f6b80dec46285affd4801
viktoria <kingsviktory@gmail.com>
2024-02-29 17:43:48 +0300

    fix: percentage class and command invoking

--- src/commands/Percentage.js
+++ src/commands/Percentage.js
@@ -1,14 +1,17 @@
 export default class Percentage {
-    constructor() {
+    constructor(valToCountPercent) {
+        this.valToCountPercent = valToCountPercent
         this.percent = 100
     }
+
     execute(currVal) {
+        console.log('percent rate',this.percent, 'koef:' , this.valToCountPercent / this.percent, 'curr val:', currVal, 'valToCountPercent', this.valToCountPercent)
         if(currVal == 0) return currVal;
-        return currVal / this.percent
+        return this.valToCountPercent / this.percent
     }
 
     undo(currVal) {
         if(currVal == 0) return currVal;
-        return currVal * this.percent;
+        return this.valToCountPercent * this.percent
     }
   }
--- src/index.js
+++ src/index.js
@@ -4,59 +4,100 @@ import Substraction from "./commands/Substraction";
 import Multiply from "./commands/Multiply";
 import Divide from "./commands/Divide";
 import SighChange from "./commands/SighChange";
+import Percentage from "./commands/Percentage";
 import { OPERATIONS, numbers } from './const';
 import './style.css'
 const btns = document.getElementById('numBoard');
+const resultPlace = document.getElementById('currentVal')
+const sequencePlace = document.getElementById('sequence')
 const calculator = new Calculator();
 console.log('initial:', calculator.val)
+
+const commands = new Map([
+    ['+', Sum],
+    ['/', Divide],
+    ['*', Multiply],
+    ['-', Substraction],
+])
+
+const basicOperators = ['+', '-', '/', '*']
 
 let a = 0
 let operator = 0
 let b = 0
 let isCompleted = false
+let shouldCalculate = false
+
 function invokeCommand(e) {
-    if (!numbers.includes(e.target.value) && e.target.value !== OPERATIONS.equal) operator = e.target.value
-    if(numbers.includes(e.target.value)){
+    console.log(isCompleted)
+    if (basicOperators.includes(e.target.value)) operator = e.target.value
+
+    if(e.target.value !== OPERATIONS.ac && e.target.value !== OPERATIONS.equal) sequencePlace.value += e.target.value;
+
+    if (numbers.includes(e.target.value)) {
         if (!b && !operator) {
             a += e.target.value
             calculator.setValue(+a)
-        }else if(a && b && isCompleted){
-            b = e.target.value
-        } else {b += e.target.value}
-    }
-
-if(a && operator && !isCompleted){
-    console.log(calculator.val, operator,+b)
-    switch(operator){
-        case (OPERATIONS.oppositeSign):
-            calculator.initialVal = calculator.executeCommand(new SighChange())
-            break
-    }
-
-    if(b){
-        switch (operator) {
-        case (OPERATIONS.equal): 
-        case (OPERATIONS.sum): 
-        calculator.executeCommand(new Sum(+b))
-            console.log( 'after sum',calculator.val)
-            b=0
-            break
-        case (OPERATIONS.substraction):
-            calculator.setValue(calculator.executeCommand(new Substraction(+b)))
-            b=0
-            break
-        case (OPERATIONS.multiply):
-            calculator.initialVal = calculator.executeCommand(new Multiply(+b))
-            b=0
-            break
-        case (OPERATIONS.division):
-            calculator.initialVal = calculator.executeCommand(new Divide(+b))
-            b=0
-            break
+        } else if (a && b && isCompleted) {
+            b += e.target.value
+            isCompleted = false
+        } else {
+            b += e.target.value
         }
     }
 
-}
+    if (a && operator && b && !numbers.includes(e.target.value)) {
+        shouldCalculate = true
+    }
+
+    if (e.target.value === OPERATIONS.percentage) {
+        a = calculator.val
+        calculator.executeCommand(new Percentage(+b))
+        b = calculator.val
+        calculator.setValue(a)
+
+        if (operator === OPERATIONS.sum || operator === OPERATIONS.substraction) {
+            const Command = commands.get(operator)
+            const num = a * b
+            calculator.executeCommand(new Command(num))
+            b = 0
+        }
+
+        resultPlace.value = calculator.val
+    }
+
+    if(e.target.value === OPERATIONS.ac) {
+        calculator.setValue(0)
+        resultPlace.value = ''
+        sequencePlace.value = ''
+        isCompleted = false
+        shouldCalculate = false
+    }
+
+    if(e.target.value === OPERATIONS.oppositeSign){
+        calculator.executeCommand(new SighChange())
+    }
+
+    if (a && b && shouldCalculate) {
+        if (e.target.value === OPERATIONS.equal) {
+            console.log(commands.get(operator))
+            const Command = commands.get(operator)
+            calculator.executeCommand(new Command(+b))
+            console.log('triggered equal', calculator.val, operator)
+            sequencePlace.value = ''
+            resultPlace.value = calculator.val
+            a = calculator.val
+            b = 0
+            isCompleted = true
+            shouldCalculate = false
+        }
+        const command = commands.get(operator)
+        calculator.executeCommand(new command(+b))
+        console.log('triggered command:', commands.get(operator), calculator.val)
+        b = 0
+        isCompleted = true
+        resultPlace.value = calculator.val
+    }
 
 }
 btns.addEventListener('click', (e) => invokeCommand(e))
