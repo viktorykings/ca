b88b0a3469bff47112072a14b5301fdd443f61cd
viktoria <kingsviktory@gmail.com>
2024-03-02 18:14:10 +0300

    feat: handle error while divide by zero

--- src/commands/Divide.js
+++ src/commands/Divide.js
@@ -3,7 +3,15 @@ export default class Divide {
     this.valToDivide = valToDivide;
   }
 
+  _toFixedWithoutZeros = (num, precision = 8) => Number.parseFloat(num.toFixed(precision));
+
   execute(currVal) {
-    return currVal / this.valToDivide;
+    if (Number.isFinite(parseFloat(currVal) / parseFloat(this.valToDivide))) {
+      if (Number.isInteger(parseFloat(currVal) / parseFloat(this.valToDivide))) {
+        return parseFloat(currVal) / parseFloat(this.valToDivide);
+      }
+      return this._toFixedWithoutZeros((parseFloat(currVal) / parseFloat(this.valToDivide)));
+    }
+    return "Error";
   }
 }
